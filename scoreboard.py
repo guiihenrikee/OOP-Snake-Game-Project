e88c35f51829f6a80131da9c2790082b8711593d
Guilherme Henrique Rosa <74630279+guiihenrikee@users.noreply.github.com>
2021-04-22 14:59:21 -0300

    Update scoreboard.py
    Update on scoreboard, now can track the highscore.

--- scoreboard.py
+++ scoreboard.py
@@ -7,6 +7,7 @@ class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
+        self.high_score = 0
         self.color("white")
         self.penup()
         self.goto(0, 270)
@@ -14,14 +15,16 @@ class Scoreboard(Turtle):
         self.updade_scoreboard()
 
     def updade_scoreboard(self):
-        self.write(arg=f"Score: {self.score}", align=ALIGNMENT, font=FONT)
+        self.clear()
+        self.write(arg=f"Score: {self.score} High Score: {self.high_score}", align=ALIGNMENT, font=FONT)
 
 
     def increase_Score(self):
         self.score += 1
-        self.clear()
         self.updade_scoreboard()
 
-    def game_over(self):
-        self.goto(0, 0)
-        self.write("GAME OVER", align=ALIGNMENT, font=FONT)
+    def reset(self):
+        if self.score > self.high_score:
+            self.high_score = self.score
+        self.score = 0
+        self.updade_scoreboard()
